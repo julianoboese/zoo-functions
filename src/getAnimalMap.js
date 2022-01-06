78a0d17ceb67b7fa022df5691663c6ca2f6a874f
Juliano Boese <juliano.boese@gmail.com>
2022-01-06 20:37:32 -0300

    Requisito 7 - Implementa a função getAnimalMap

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,38 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+function getNames(species, sex, sorted) {
+  let names;
+
+  if (sex) {
+    names = species.residents.filter((resident) => resident.sex === sex)
+      .map((resident) => resident.name);
+  } else {
+    names = species.residents.map((resident) => resident.name);
+  }
+
+  if (sorted) {
+    names.sort();
+  }
+
+  return names;
+}
+
+function getAnimalMap({ includeNames, sex, sorted } = {}) {
+  const animalMap = data.species.reduce((acc, species) => {
+    acc[species.location] = [];
+    return acc;
+  }, {});
+
+  if (!includeNames) {
+    data.species.forEach((species) => animalMap[species.location].push(species.name));
+    return animalMap;
+  }
+
+  data.species.forEach((species) => {
+    animalMap[species.location].push({ [species.name]: getNames(species, sex, sorted) });
+  });
+
+  return animalMap;
 }
 
 module.exports = getAnimalMap;
