c5f4389206415c4ad2389a5cbca7c0aa217bf047
Juliano Boese <juliano.boese@gmail.com>
2022-01-06 15:30:50 -0300

    Requisito 2 - Implementa a função getAnimalsOlderThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return data.species.find((species) => species.name === animal)
+    .residents.every((resident) => resident.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
