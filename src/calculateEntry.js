07518a65e249de8cde9fc5343c407f31ca82d8bd
Juliano Boese <juliano.boese@gmail.com>
2022-01-06 18:05:25 -0300

    Requisito 6 - Implementa a função calculateEntry

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,25 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const totalEntrants = { child: 0, adult: 0, senior: 0 };
+  if (Object.keys(entrants).length === 0) {
+    return totalEntrants;
+  }
+  entrants.forEach((entrant) => {
+    if (entrant.age < 18) {
+      totalEntrants.child += 1;
+    } else if (entrant.age < 50) {
+      totalEntrants.adult += 1;
+    } else {
+      totalEntrants.senior += 1;
+    }
+  });
+  return totalEntrants;
 }
 
-function calculateEntry(entrants) {
-  // seu código aqui
+function calculateEntry(entrants = []) {
+  const { child, adult, senior } = countEntrants(entrants);
+  return child * data.prices.child + adult * data.prices.adult + senior * data.prices.senior;
 }
 
 module.exports = { calculateEntry, countEntrants };
