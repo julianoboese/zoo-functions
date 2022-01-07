81fe9cd3e6a7982b4356cabb79466099b19cbd4a
Juliano Boese <juliano.boese@gmail.com>
2022-01-07 00:18:59 -0300

    Requisito 10 - Implementa a função getEmployeesCoverage

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,41 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function createCompleteCoverage() {
+  return data.employees.map((employee) => {
+    const animals = data.species.filter((species) => employee.responsibleFor.includes(species.id));
+    return {
+      id: employee.id,
+      fullName: `${employee.firstName} ${employee.lastName}`,
+      species: animals.map((species) => species.name),
+      locations: animals.map((species) => species.location),
+    };
+  });
+}
+
+function getPerson(id, name) {
+  if (id) {
+    return createCompleteCoverage().find((employee) => employee.id === id);
+  }
+  if (name) {
+    return createCompleteCoverage().find((employee) => employee.fullName.split(' ').includes(name));
+  }
+}
+
+function getEmployeesCoverage({ id, name } = {}) {
+  if (!id && !name) {
+    return createCompleteCoverage();
+  }
+
+  const person = getPerson(id, name);
+
+  try {
+    if (person) {
+      return person;
+    }
+    throw new Error('Informações inválidas');
+  } catch (error) {
+    throw error.message;
+  }
 }
 
 module.exports = getEmployeesCoverage;
