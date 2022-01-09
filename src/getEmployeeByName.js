411317496a3bb90cceaa0b872031656220b511aa
Juliano Boese <juliano.boese@gmail.com>
2022-01-09 12:06:04 -0300

    getEmployeeByName - melhora retorno em caso de find undefined

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -3,7 +3,7 @@ const data = require('../data/zoo_data');
 function getEmployeeByName(employeeName) {
   const employeeObject = data.employees.find((employee) => employee.firstName === employeeName
   || employee.lastName === employeeName);
-  return employeeObject === undefined ? {} : employeeObject;
+  return employeeObject || {};
 }
 
 module.exports = getEmployeeByName;
