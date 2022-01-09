d7298df00063ad6f57cea60c58118bed74324c5d
Juliano Boese <juliano.boese@gmail.com>
2022-01-09 13:16:36 -0300

    Refatora getAnimalMap

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,14 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getNames(species, sex, sorted) {
-  let names;
-
-  if (sex) {
-    names = species.residents.filter((resident) => resident.sex === sex)
-      .map((resident) => resident.name);
-  } else {
-    names = species.residents.map((resident) => resident.name);
-  }
+  const names = sex ? species.residents.filter((resident) => resident.sex === sex)
+    .map((resident) => resident.name)
+    : species.residents.map((resident) => resident.name);
 
   if (sorted) {
     names.sort();
@@ -18,20 +13,11 @@ function getNames(species, sex, sorted) {
 }
 
 function getAnimalMap({ includeNames, sex, sorted } = {}) {
-  const animalMap = data.species.reduce((acc, species) => (
-    { ...acc, [species.location]: [] }
+  return data.species.reduce((acc, species) => (
+    { ...acc,
+      [species.location]: !includeNames ? [...acc[species.location] || '', species.name]
+        : [...acc[species.location] || '', { [species.name]: getNames(species, sex, sorted) }] }
   ), {});
-
-  if (!includeNames) {
-    data.species.forEach((species) => animalMap[species.location].push(species.name));
-    return animalMap;
-  }
-
-  data.species.forEach((species) => {
-    animalMap[species.location].push({ [species.name]: getNames(species, sex, sorted) });
-  });
-
-  return animalMap;
 }
 
 module.exports = getAnimalMap;
