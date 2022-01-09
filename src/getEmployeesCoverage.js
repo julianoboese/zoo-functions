ca0ff387f1b575d99b12926c334e0c932d203f8d
Juliano Boese <juliano.boese@gmail.com>
2022-01-09 18:17:25 -0300

    Refatora getEmployeesCoverage

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,29 +1,20 @@
 const { species, employees } = require('../data/zoo_data');
 
-function createCompleteCoverage() {
-  return employees.map(({ id: empId, firstName, lastName, responsibleFor }) => {
-    const animals = species.filter(({ id: specId }) => responsibleFor.includes(specId));
-    return {
-      id: empId,
-      fullName: `${firstName} ${lastName}`,
-      species: animals.map(({ name }) => name),
-      locations: animals.map(({ location }) => location),
-    };
-  });
-}
-
 function getEmployeesCoverage({ id, name } = {}) {
-  if (!id && !name) {
-    return createCompleteCoverage();
-  }
+  const people = employees.map(({ id: empId, firstName, lastName, responsibleFor }) => {
+    const animals = species.filter(({ id: specId }) => responsibleFor.includes(specId));
+    return { id: empId,
+      fullName: `${firstName} ${lastName}`,
+      species: animals.map(({ name: specName }) => specName),
+      locations: animals.map(({ location }) => location) };
+  });
 
-  const person = createCompleteCoverage().find(({ id: pId, fullName }) => pId === id
-  || fullName.includes(name));
+  if (!id && !name) return people;
+
+  const person = people.find(({ id: pId, fullName }) => pId === id || fullName.includes(name));
 
   try {
-    if (person) {
-      return person;
-    }
+    if (person) return person;
     throw new Error('Informações inválidas');
   } catch (error) {
     throw error.message;
