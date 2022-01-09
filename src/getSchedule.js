9fdb7c5452265537a50f7123e4aaa58dc33f1d45
Juliano Boese <juliano.boese@gmail.com>
2022-01-09 17:57:19 -0300

    Refatora getSchedule

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,37 +1,22 @@
 const { species, hours } = require('../data/zoo_data');
 
-function createCompleteSchedule() {
-  const schedule = {};
+function getSchedule(scheduleTarget) {
+  if (species.some(({ name }) => name === scheduleTarget)) {
+    return species.find(({ name }) => name === scheduleTarget).availability;
+  }
 
-  Object.keys(hours).forEach((weekday) => {
-    const animals = [];
-    species.forEach((spec) => {
-      if (spec.availability.includes(weekday)) {
-        animals.push(spec.name);
-      }
-    });
-    schedule[weekday] = {
-      officeHour: `Open from ${hours[weekday].open}am until ${hours[weekday].close}pm`,
-      exhibition: animals,
-    };
-  });
+  const schedule = Object.entries(hours).reduce((acc, [weekday, { open, close }]) =>
+    ({ ...acc,
+      [weekday]: {
+        officeHour: `Open from ${open}am until ${close}pm`,
+        exhibition: species.reduce((acc2, { name, availability }) =>
+          (availability.includes(weekday) ? [...acc2, name] : acc2), []),
+      },
+    }),
+  {});
   schedule.Monday = { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
 
-  return schedule;
-}
-
-function getSchedule(scheduleTarget) {
-  if (species.some((spec) => spec.name === scheduleTarget)) {
-    return species.find((spec) => spec.name === scheduleTarget).availability;
-  }
-
-  const schedule = createCompleteSchedule();
-
-  if (schedule[scheduleTarget]) {
-    return { [scheduleTarget]: schedule[scheduleTarget] };
-  }
-
-  return schedule;
+  return schedule[scheduleTarget] ? { [scheduleTarget]: schedule[scheduleTarget] } : schedule;
 }
 
 module.exports = getSchedule;
